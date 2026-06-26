0d2cfd542710e83f8c3bd19841d7b7b75f0478b6
Oshani Lakchani <oshanilakchani@gmail.com>
2026-06-26 23:05:17 +0530

    Add test for expiry alert validation on CertificationPage

--- Cypress_Automation/cypress/pages/CertificationPage.js
+++ Cypress_Automation/cypress/pages/CertificationPage.js
@@ -1,1 +1,15 @@
+import CertificationPage from "../pages/CertificationPage";
 
+describe("Certification Tracking", () => {
+
+  it("Expiry Alert Validation", () => {
+
+    cy.login("admin@test.com", "Admin@123");
+
+    CertificationPage.visit();
+
+    CertificationPage.checkExpiryAlert()
+      .should("contain", "Expiring within 30 days");
+  });
+
+});
